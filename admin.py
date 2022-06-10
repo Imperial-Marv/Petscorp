555c2b711996cd6028a72d1cb6c86dea75c4a7a9
Imperial-Marv <101286163+Imperial-Marv@users.noreply.github.com>
2022-06-10 04:17:16 -0400

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from distutils import core
-from django.contrib import admin
-from .models import  Usuario
-from .models import  Producto
-
-# Register your models here.
-
-admin.site.register(Usuario)
-admin.site.register(Producto)
